283c45d2f0d089bbb986d2a81b0c2a9f598cedcf
Hassan Lash <hlasheen21@gmail.com>
2022-12-05 14:41:23 +0200

    Hash tables in Python

--- GrokkingAlgorithms.py
+++ GrokkingAlgorithms.py
@@ -6,48 +6,48 @@
 # Big O notation doesn't tell the speed in seconds it tells how fast the algorithms grows
 
 
-# def binary_search(list, item):
-#     low = 0
-#     high = len(list) - 1
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
 
-#     while low <= high:
-#         mid = (low + high)
-#         guess = list[mid]
-#         if guess == item:
-#             return mid
-#         if guess > item:
-#             high = mid - 1
-#         else:
-#             low = mid + 1
-#     return None
+    while low <= high:
+        mid = (low + high)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
 
-# my_list = [1, 3, 6, 5, 7, 8]
+my_list = [1, 3, 6, 5, 7, 8]
 
-# print (binary_search(my_list, 3))
-# print (binary_search(my_list, -1))
+print (binary_search(my_list, 3))
+print (binary_search(my_list, -1))
 
 
 # Selection sort // arrays and linked lists 
 
 # Quick sort is faster sorting algorithm than Selection sort
 
-# def findSmallest(arr):
-#     smallest = arr[0]
-#     smallest_index = 0
-#     for i in range(1, len(arr)):
-#         if arr[i] < smallest:
-#             smallest = arr[i]
-#             smallest_index = i
-#         return smallest_index
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+        return smallest_index
 
-# def selectionSort(arr):
-#     newArr = []
-#     for i in range(len(arr)):
-#         smallest = findSmallest(arr)
-#         newArr.append(arr.pop(smallest))
-#     return newArr
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
 
-# print (selectionSort([5, 3, 6, 2, 10]))
+print (selectionSort([5, 3, 6, 2, 10]))
 # 
 # arrays allow fast reads 
 # linked lists allow fast inserts and deletes
@@ -57,12 +57,12 @@
 # Recursion =>  a function that calls itself
 # many important algorithms use recursions
 
-# def look_for_key(box):
-#     for item in box:
-#         if item.is_a_box():
-#             look_for_key(item)
-#         elif item.is_a_key():
-#             print("found the key")
+def look_for_key(box):
+    for item in box:
+        if item.is_a_box():
+            look_for_key(item)
+        elif item.is_a_key():
+            print("found the key")
 
 # base case and recursive case
 
@@ -73,12 +73,12 @@
 
 # countdown function 
 
-# def countdown(i):
-#     print(i)
-#     if i<=0:
-#         return  #return nothing / stop
-#     else:
-#         countdown(i-1)
+def countdown(i):
+    print(i)
+    if i<=0:
+        return  #return nothing / stop
+    else:
+        countdown(i-1)
 
         
 # Stack #
@@ -94,13 +94,13 @@
 
 # the general technique is divide and conquer and quick sort uses this. 
 
-# def sum(arr):
-#     total = 0
-#     for x in arr:
-#         total += x
-#     return total
+def sum(arr):
+    total = 0
+    for x in arr:
+        total += x
+    return total
 
-# print(sum([1, 2, 3, 4]))
+print(sum([1, 2, 3, 4]))
 
 
 # recursive function example
@@ -115,90 +115,90 @@
 
 # recursive funtion to count numbers in list 
 
-# def count_rec(lst):
-#     if not lst:
-#         return 0
-#     return 1 + count_rec(lst[1:])
+def count_rec(lst):
+    if not lst:
+        return 0
+    return 1 + count_rec(lst[1:])
 
-# print(count_rec([1, 5, 4, 9]))
+print(count_rec([1, 5, 4, 9]))
 
-    # find the maximum number in a list #
+    # find the maximum number in a list 
 
-# flats = [100, 2, 300, 10, 11, 1000]
+flats = [100, 2, 300, 10, 11, 1000]
 
-# largest_num = flats[0]
+largest_num = flats[0]
 
-# for number in flats:
-#     if number > largest_num:
-#         largest_num = number
-# print(largest_num)
+for number in flats:
+    if number > largest_num:
+        largest_num = number
+print(largest_num)
 
-    # or we can use a max() #
+    # or we can use a max() 
 
-# flats = [100, 2, 300, 10, 11, 1000]
+flats = [100, 2, 300, 10, 11, 1000]
 
-# max_number = max(flats)
+max_number = max(flats)
 
-# print(max_number)
+print(max_number)
 
         
-#### how to quick sort an array using Divide & Conquer ####
-# def quicksort(array):
-#     if len(array) < 2:
-#         return array
-#     else:
-#         pivot = array[0]
-#         less = [i for i in array[1:] if i <= pivot]
-#         greater = [i for i in array[1:] if i > pivot]
+### how to quick sort an array using Divide & Conquer ####
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
 
-#         return quicksort(less) + [pivot] + quicksort(greater)
-# print(quicksort([10, 5, 2, 3]))
+        return quicksort(less) + [pivot] + quicksort(greater)
+print(quicksort([10, 5, 2, 3]))
 
 
             # Hash Tables # => maps keys to values 
 
-# book = dict()   #dictionaries in Python are hash tables
-# book["apple"] = 0.67
-# book["milk"] = 1.49
-# book["avocado"] = 1.49
-# print(book)
-# print(book["avocado"])
+book = dict()   #dictionaries in Python are hash tables
+book["apple"] = 0.67
+book["milk"] = 1.49
+book["avocado"] = 1.49
+print(book)
+print(book["avocado"])
 
 # build a phone book hash tables 
-#use curly braces as a shortcut to dict()
-# phone_book = {
-#     "Jenny": 8675309,
-#     "emergency": 911
-# }
+# use curly braces as a shortcut to dict()
+phone_book = {
+    "Jenny": 8675309,
+    "emergency": 911
+}
 
-# print(phone_book["Jenny"]);
+print(phone_book["Jenny"]);
 
 # create a hash to keep track of people who voted
 
-# voted = {}
+voted = {}
 
-# def check_voter(name):
-#     if voted.get(name):
-#         print("Kick him out!")
-#     else:
-#         voted[name] = True
-#         print("Let them vote!")
+def check_voter(name):
+    if voted.get(name):
+        print("Kick him out!")
+    else:
+        voted[name] = True
+        print("Let them vote!")
 
 
-# check_voter("Mike")
-# check_voter("Mike")
+check_voter("Mike")
+check_voter("Mike")
 
         # create a caching #
 
-# cache = {}
+cache = {}
 
-# def get_page(url):
-#     if cache.get(url):
-#         return cache[url]
-#     else:
-#         data = get_data_from_server(url)
-#         cache[url] = data
-#         return data
+def get_page(url):
+    if cache.get(url):
+        return cache[url]
+    else:
+        data = get_data_from_server(url)
+        cache[url] = data
+        return data
 
 
 
